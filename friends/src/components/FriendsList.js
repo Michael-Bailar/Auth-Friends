16b1623d80726e55875eb002a38b99be5c7ae4b9
Michael-B1764 <mikebailar@gmail.com>
2020-03-24 13:43:23 -0700

    working

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FriendsList = () => {
+    return(
+        <div>friends list test</div>
+    )
+}
+
+export default FriendsList
---
+++ friends/src/components/Login.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import axiosWithAuth from '../utils/axiosWithAuth'
+
+const Login = props => {
+    //set state
+    const [credentials, setCredentials] = useState({username: '', password: ''})
+    //declare functions
+    const handleChange = e => {
+        
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+        console.log('Login credentials', credentials)
+        axios
+            .post('http://localhost:5000/api/login', credentials)
+            .then(res => {
+                console.log(res)
+                localStorage.setItem('token', res.data.payload)
+                props.history.push('/friendslist')
+            })
+            .catch(err => console.log(err.res))
+    }
+
+    //render component
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    name='username'
+                    value={credentials.username}
+                    onChange={handleChange}
+                />
+                <input 
+                    type='password'
+                    name='password'
+                    value={credentials.password}
+                    onChange={handleChange}
+                />
+                <button>Log in</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
